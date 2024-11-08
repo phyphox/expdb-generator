26c0fbf480c698b9a2f7e1bed0c0b8862b159d57
RWTH-MK <marcel.kaiser1@rwth-aachen.de>
2024-11-08 13:07:16 +0100

    test git action with script as file

---
+++ pandoc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Only_Standalone_Output_Types="latex pdf html docx odt"
+
+parallel --jobs 0 \
+	pandoc --from markdown --to native {} -o '{.}.ast' ';'\
+	for i in "$Only_Standalone_Output_Types"';' do \
+		pandoc --from native '{.}.ast' --standalone -o '{.}.$i' ';' \
+    done';' \
+	rm '{.}.ast' ::: "$1"

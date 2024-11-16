c97dbb1dbda7c4d81bb7e904f06bfaeae59e33ed
RWTH-MK <marcel.kaiser1@rwth-aachen.de>
2024-11-16 15:48:48 +0100

    test git action

--- pandoc.sh
+++ pandoc.sh
@@ -4,6 +4,7 @@ Only_Standalone_Output_Types="latex pdf html docx odt"
 # Use GNU Parallel to work each file on its own cpu core.
 # Pandoc creates an AST (Abstract Syntax Tree); reuse this by saving/reading from .ast
 # You can easily add individual conversion rules by using pandoc after the 'done' part. Keep in mind to finish all non-comment lines with backslash
+echo "You found me!"
 parallel --jobs 0 \
 	pandoc --from markdown --to native './{}' -o './{.}.ast' ';'\
 	for i in "$Only_Standalone_Output_Types"';' do \

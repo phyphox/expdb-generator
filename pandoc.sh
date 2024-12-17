91b60c68d0138465f72acd98d6e038805ab1a198
Staacks <staacks@physik.rwth-aachen.de>
2024-12-17 16:38:41 +0100

    Switch to LuaLaTeX

--- pandoc.sh
+++ pandoc.sh
@@ -6,6 +6,6 @@ Only_Standalone_Output_Types="latex pdf html docx odt"
 parallel --jobs 0 \
 	pandoc --from markdown --to native './{}' -o './{.}.ast' ';'\
 	for i in "$Only_Standalone_Output_Types"';' do \
-		pandoc --from native './{.}.ast' --standalone -o './{.}.$i' ';' \
+		pandoc --from native './{.}.ast' --standalone --pdf-engine=lualatex -o './{.}.$i' ';' \
     done';' \
 	rm './{.}.ast' ::: "$1"

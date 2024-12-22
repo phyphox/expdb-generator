203f6d9c974fd4d2123f253f0f98d1e563328c9c
Staacks <staacks@physik.rwth-aachen.de>
2024-12-22 23:28:02 +0100

    Add ungroup to parallel invocation as grouping does not allow to see which pandoc conversion generated errors or warnings.

--- pandoc.sh
+++ pandoc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Just a wrapper for parallelization, but separating this into its own script saves some nasty escaping escapates.
-parallel --jobs 0 ./pandoc-generator/pandoc-single.sh ::: "$@"
+parallel --jobs 0 --ungroup ./pandoc-generator/pandoc-single.sh ::: "$@"

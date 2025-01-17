8f0747c3f832af91ec057edfa7fa2161fa611353
Staacks <staacks@physik.rwth-aachen.de>
2025-01-17 16:55:58 +0100

    Fix: Instructions are now leafs (in the Hugo sense) and pandoc should include images from same path as md files.

--- pandoc-generator/pandoc-single.sh
+++ pandoc-generator/pandoc-single.sh
@@ -5,32 +5,33 @@ Only_Standalone_Output_Types="latex docx odt"
 # You can add individual pandoc conversions by calling them after the 'done' part. Keep in mind to finish all lines with backslash
 
 FILENAME=$(basename -- "$1")
-LOGID="${FILENAME%.*}"
+LOGID="$(basename -- "$(dirname -- "$1")")"
 SUPPORTDIR=$(dirname "$0")
 SRCDIR=$(dirname "$1")
 SRCDIR=${SRCDIR#./}
 I18NSRCDIR=${SRCDIR#en/}
+INTERMEDIATE="../$I18NSRCDIR/$LOGID"
 OUTFILE="../$I18NSRCDIR/generated/$LOGID"
 
 mkdir -p "../$I18NSRCDIR/generated"
 
 echo "PANDOC Processing $1 as $LOGID"
 
-pandoc --from markdown+tex_math_single_backslash --to native "content/$1" -o "$OUTFILE.ast"
+pandoc --resource-path ".:../$I18NSRCDIR" --from markdown+tex_math_single_backslash --to native "content/$1" -o "$INTERMEDIATE.ast"
 for ext in ${Only_Standalone_Output_Types}; do
     echo "$LOGID Generating $ext"
-    pandoc --from native "$OUTFILE.ast" --standalone --pdf-engine=lualatex -o "$OUTFILE.$ext"
+    pandoc --resource-path ".:../$I18NSRCDIR" --from native "$INTERMEDIATE.ast" --standalone --pdf-engine=lualatex -o "$OUTFILE.$ext"
 done
 
 # HTML
 echo "$LOGID Generating pdf"
-pandoc --from native "$OUTFILE.ast" --standalone --mathml -o "$OUTFILE.html"; \
+pandoc --resource-path ".:../$I18NSRCDIR" --from native "$INTERMEDIATE.ast" --standalone --mathml -o "$OUTFILE.html"; \
 
 # PDF
 echo "$LOGID Generating pdf"
-pandoc --from native "$OUTFILE.ast" --standalone -H "$SUPPORTDIR/preamble.tex" --pdf-engine=pdflatex -o "$OUTFILE.pdf"; \
+pandoc --resource-path ".:../$I18NSRCDIR" --from native "$INTERMEDIATE.ast" --standalone -H "$SUPPORTDIR/preamble.tex" --pdf-engine=pdflatex -o "$OUTFILE.pdf"; \
 
-echo "$LOGID Cleaning up"
-rm "$OUTFILE.ast"
+echo "$LOGID Cleaning up $INTERMEDIATE.ast"
+rm "$INTERMEDIATE.ast"
 
 echo "$LOGID Done."
